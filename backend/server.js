3bce8f862e98c7d00b25591b1772d2f037e37f2b
WYZ00 <thanthtettin@gmail.com>
2023-08-21 23:28:07 +0630

    create folders and home route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,14 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
