c4c50e02cca5075ea3a72bed9d6c6310e0ff4bb6
WYZ00 <thanthtettin@gmail.com>
2023-08-23 19:36:41 +0630

    create updateuser routes and controller

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,6 +5,7 @@ const {
   logoutUser,
   getUser,
   loginStatus,
+  updateUser,
 } = require("../controllers/userController");
 const protect = require("../middleWares/authMiddleWare");
 const router = express.Router();
@@ -14,5 +15,6 @@ router.post("/login", loginUser);
 router.get("/logout", logoutUser);
 router.get("/getuser", protect, getUser);
 router.get("/loggedin", loginStatus);
+router.patch("/updateuser", protect, updateUser);
 
 module.exports = router;
